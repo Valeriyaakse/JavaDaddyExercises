ffbaa864f5cdda8a4f5642ff26938ca968ce3e67
valeriya <valeriyaakse@gmail.com>
2025-08-14 13:17:07 +0300

    ForLoopPrint task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic4_Loops/task4_ForLoopPrint/ForLoopPrint.java
@@ -0,0 +1,13 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic4_Loops.task4_ForLoopPrint;
+
+public class ForLoopPrint {
+    public static void main(String[] args) {
+        forLoopPrint();
+    }
+    public static void forLoopPrint() {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
