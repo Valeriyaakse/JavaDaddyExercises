41d41d9a64fef4e586a068716819cf28ad170db8
valeriya <valeriyaakse@gmail.com>
2025-08-14 12:01:25 +0300

    add Hello World

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic1_Console/task1_HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic1_Console.task1_HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+    static void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+}
