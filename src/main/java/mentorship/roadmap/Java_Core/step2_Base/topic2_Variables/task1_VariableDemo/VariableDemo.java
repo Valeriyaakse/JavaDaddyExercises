6fc7bafec391ca9238bf494e6a60b94fb51d61b9
valeriya <valeriyaakse@gmail.com>
2025-08-14 12:37:54 +0300

    EchoWithMessage task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic2_Variables/task1_VariableDemo/VariableDemo.java
@@ -0,0 +1,13 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic2_Variables.task1_VariableDemo;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 2.3;
+        boolean c = true;
+        variableDemo(a, b, c);
+    }
+    public static void variableDemo(int a, double b,boolean c) {
+        System.out.println(a + " " + b + " " + c);
+    }
+}
