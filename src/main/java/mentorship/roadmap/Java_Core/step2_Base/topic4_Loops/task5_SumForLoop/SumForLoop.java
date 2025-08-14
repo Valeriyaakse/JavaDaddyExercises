4c7801c8f4aea044332422dda555e1259f8efadb
valeriya <valeriyaakse@gmail.com>
2025-08-14 13:24:27 +0300

    SumForLoop task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic4_Loops/task5_SumForLoop/SumForLoop.java
@@ -0,0 +1,14 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic4_Loops.task5_SumForLoop;
+
+public class SumForLoop {
+    public static void main(String[] args) {
+        sumForLoop();
+    }
+    public static void sumForLoop() {
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
