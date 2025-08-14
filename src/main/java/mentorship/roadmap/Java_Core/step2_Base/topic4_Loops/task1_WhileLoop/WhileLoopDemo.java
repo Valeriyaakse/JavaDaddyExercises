646c5e95865f67f337de55958cb60091b6727bf4
valeriya <valeriyaakse@gmail.com>
2025-08-14 13:15:21 +0300

    WhileLoopDemo task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic4_Loops/task1_WhileLoop/WhileLoopDemo.java
@@ -0,0 +1,14 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic4_Loops.task1_WhileLoop;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        whileLoopDemo();
+    }
+    public static void whileLoopDemo() {
+        int i = 1;
+        while (i <= 1000) {
+            System.out.println(i);
+            i+=1;
+        }
+    }
+}
