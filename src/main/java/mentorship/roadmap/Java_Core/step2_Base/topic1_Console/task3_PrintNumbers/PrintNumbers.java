e5a8671a04bfdb38880d1d035c0cf6d9914f9152
valeriya <valeriyaakse@gmail.com>
2025-08-14 12:26:57 +0300

    PrintMessage task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic1_Console/task3_PrintNumbers/PrintNumbers.java
@@ -0,0 +1,12 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic1_Console.task3_PrintNumbers;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        printNumbers();
+    }
+    static void printNumbers() {
+        for (int i = 1; i<=5; i++) {
+            System.out.println(i);
+        }
+    }
+}
