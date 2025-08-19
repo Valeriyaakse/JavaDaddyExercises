39560f47ca718fe163615054dd49257218768d3f
valeriya <valeriyaakse@gmail.com>
2025-08-19 17:07:48 +0300

    Car task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic4_Car/CarDemo.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic4_Car;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car car1 = new Car("BMV", "M5", 2020);
+        car1.displayInfo();
+    }
+}
