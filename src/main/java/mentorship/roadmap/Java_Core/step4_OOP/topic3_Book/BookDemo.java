19b1e29170d34da7567871da94a5c5ce74a77378
valeriya <valeriyaakse@gmail.com>
2025-08-19 16:13:06 +0300

    Book task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic3_Book/BookDemo.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic3_Book;
+
+public class BookDemo {
+    public static void main( String[] args) {
+        Book book1 = new Book("Преступление и наказание", "Ф. И. Достоевский", 1866);
+        System.out.println(book1.toString());
+}
+}
