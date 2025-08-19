da669eb6dc5d05ce3c96aa01b8b8ecfa91e5f1a6
valeriya <valeriyaakse@gmail.com>
2025-08-19 15:07:43 +0300

    Person task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic1_Person/PersonDemo.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic1_Person;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person person1 = new Person("Tom", 22);
+        person1.displayInfo();
+    }
+}
