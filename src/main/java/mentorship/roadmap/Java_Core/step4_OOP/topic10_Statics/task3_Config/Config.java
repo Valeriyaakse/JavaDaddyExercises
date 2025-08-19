615239f37737bc68a527d0e4b056df77f29cb1ab
valeriya <valeriyaakse@gmail.com>
2025-08-19 23:13:22 +0300

    Config task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic10_Statics/task3_Config/Config.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic10_Statics.task3_Config;
+
+public class Config {
+    private static String configVersion = "v1.0.0";
+    public static void displayConfig() {
+        System.out.println(configVersion);
+    }
+}
