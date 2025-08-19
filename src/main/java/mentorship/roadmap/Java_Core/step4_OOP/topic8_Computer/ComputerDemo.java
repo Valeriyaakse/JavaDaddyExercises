b24930868d04e4f7b61a1452634e3dfe56276914
valeriya <valeriyaakse@gmail.com>
2025-08-19 18:17:24 +0300

    Computer task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic8_Computer/ComputerDemo.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic8_Computer;
+
+public class ComputerDemo {
+    public static void main(String[] args) {
+        Computer computer = new Computer("testBrand", "testModel", 123);
+        computer.displaySpecs();
+    }
+}
