136c7f625624e20d01fa3a26a615a65dc68f1b5a
valeriya <valeriyaakse@gmail.com>
2025-08-19 23:17:09 +0300

    MathConstants task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic10_Statics/task4_MathConstants/ConstantsDemo.java
@@ -0,0 +1,7 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic10_Statics.task4_MathConstants;
+
+public class ConstantsDemo {
+    public static void main(String[] args) {
+        System.out.println(MathConstants.PI + " " + MathConstants.E);
+    }
+}
---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic10_Statics/task4_MathConstants/MathConstants.java
@@ -0,0 +1,6 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic10_Statics.task4_MathConstants;
+
+public class MathConstants {
+    public final static double PI = 3.14159;
+    public final static double E = 2.71828;
+}
