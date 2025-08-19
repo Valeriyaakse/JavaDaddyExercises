bb038331c5747d87be50e6f0ac349bf93fa6cbe9
valeriya <valeriyaakse@gmail.com>
2025-08-19 22:50:48 +0300

    StringPoolDemo task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic9_String/task3_StringPoolDemo/StringPoolDemo.java
@@ -0,0 +1,9 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic9_String.task3_StringPoolDemo;
+
+public class StringPoolDemo {
+    public static void main(String[] args) {
+        String str1 = "Cat";
+        String str2 = "Cat";
+        System.out.println(str1 == str2);
+    }
+}
