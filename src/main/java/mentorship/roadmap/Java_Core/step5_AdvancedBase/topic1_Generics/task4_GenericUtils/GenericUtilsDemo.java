90738ab8124eae6359d2405809bda90c2849a4f3
valeriya <valeriyaakse@gmail.com>
2025-08-21 11:11:33 +0300

    GenericUtils task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic1_Generics/task4_GenericUtils/GenericUtilsDemo.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic1_Generics.task4_GenericUtils;
+
+public class GenericUtilsDemo {
+   public static void main(String[] args) {
+       System.out.println(GenericUtils.max(40,8));
+       System.out.println(GenericUtils.max("aaa", "aaaaa"));
+   }
+}
