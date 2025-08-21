daa5f6e2fbde9b4a45187285788afb3d85f7c4b7
valeriya <valeriyaakse@gmail.com>
2025-08-21 11:23:08 +0300

    Day task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic3_Enums/task1_Day/DayDemo.java
@@ -0,0 +1,11 @@
+package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic3_Enums.task1_Day;
+
+public class DayDemo {
+    public static void main(String[] args) {
+        Day[] values = Day.values();
+        for (Day days: values) {
+            System.out.println(days);
+        }
+
+    }
+}
