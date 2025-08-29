22db3d08c38e147e9c395173f28d905268372063
valeriya <valeriyaakse@gmail.com>
2025-08-29 15:26:38 +0300

    task1_Queue QueueDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step6_Collections/topic3_Queue/task1_Queue/QueueDemo.java
@@ -0,0 +1,20 @@
+package mentorship.roadmap.Java_Core.step6_Collections.topic3_Queue.task1_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+        queue.offer("D");
+        queue.offer("E");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+        System.out.println(queue.peek());
+
+    }
+}
