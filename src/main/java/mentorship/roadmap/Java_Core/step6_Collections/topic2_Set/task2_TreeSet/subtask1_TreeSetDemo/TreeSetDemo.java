6a89ee6c22f8323708e9c538c66e66d96b6fcd27
valeriya <valeriyaakse@gmail.com>
2025-08-29 14:36:22 +0300

    task1 TreeSet subtask1 TreeSetDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step6_Collections/topic2_Set/task2_TreeSet/subtask1_TreeSetDemo/TreeSetDemo.java
@@ -0,0 +1,16 @@
+package mentorship.roadmap.Java_Core.step6_Collections.topic2_Set.task2_TreeSet.subtask1_TreeSetDemo;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(50);
+        treeSet.add(20);
+        treeSet.add(10);
+        treeSet.add(40);
+        treeSet.add(30);
+        System.out.println(treeSet);
+
+    }
+}
