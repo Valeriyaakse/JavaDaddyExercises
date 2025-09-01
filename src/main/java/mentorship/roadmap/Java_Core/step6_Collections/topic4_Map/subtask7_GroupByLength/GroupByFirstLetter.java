9f742d95baf0cdcb2b6084a0011f9fc5f31b157b
valeriya <valeriyaakse@gmail.com>
2025-09-01 16:09:14 +0300

    task4_Map subtask4 GroupByFirstLetter

---
+++ src/main/java/mentorship/roadmap/Java_Core/step6_Collections/topic4_Map/subtask7_GroupByLength/GroupByFirstLetter.java
@@ -0,0 +1,27 @@
+package mentorship.roadmap.Java_Core.step6_Collections.topic4_Map.subtask7_GroupByLength;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupByFirstLetter {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Ваня", "Матвей","Вася","Маша","Таня", "Галя","Гоша", "Валя"};
+
+        HashMap<Character, List<String>> hashMap = new HashMap<>();
+
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+
+            if (!hashMap.containsKey(firstLetter)) {
+                hashMap.put(firstLetter, new ArrayList<>());
+            }
+
+            hashMap.get(firstLetter).add(name);
+        }
+
+        for (char key : hashMap.keySet()) {
+            System.out.println(key + " : " + hashMap.get(key));
+        }
+    }
+}
