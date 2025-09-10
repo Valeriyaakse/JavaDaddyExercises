4fd710f8f318a01b47e9db825818f0c0d3570a56
valeriya <valeriyaakse@gmail.com>
2025-09-10 11:44:04 +0300

    Module4Step3 topic2 RunnableLambdaDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step8_Multithereading/topic2_Runnable/RunnableLambdaDemo.java
@@ -0,0 +1,26 @@
+package mentorship.roadmap.Java_Core.step8_Multithereading.topic2_Runnable;
+
+public class RunnableLambdaDemo {
+    public static void main(String[] args) {
+       try {
+           Runnable task1 = () -> System.out.println("Task 1 is running");
+           Runnable task2 = () -> System.out.println("Task 2 is running");
+           Runnable task3 = () -> System.out.println("Task 3 is running");
+
+           Thread thread1 = new Thread(task1);
+           Thread thread2 = new Thread(task2);
+           Thread thread3 = new Thread(task3);
+
+           thread1.start();
+           thread2.start();
+           thread3.start();
+
+           thread1.join();
+           thread2.join();
+           thread3.join();
+       } catch (InterruptedException e) {
+           System.out.println(e.getMessage());
+       }
+
+    }
+}
